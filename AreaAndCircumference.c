e7c382dde5b1ab9211b63d9d8e895c61d0a7be0b
MeowsQueen <117544573+MeowsQueen@users.noreply.github.com>
2022-11-06 23:54:00 +0300

    Update AreaAndCircumference.c

--- AreaAndCircumference.c
+++ AreaAndCircumference.c
@@ -1,13 +1,11 @@
 // To calculate Area and Circumference of a circle
-
-#include<stdio.h>
-int main()
-{
-    float r, a ,c;
-    printf("enter radius :\n");
-    scanf("%f", &r);
-    a = 3.14*r*r;
-    c = 2*3.14*r;
-    printf("Area = %f\n circumference = %f\n",a,c);
+#include <stdio.h>
+int main() {
+    float area_circle,radius,circumference_circle;
+    printf("enter a radius value for area and circumference of the circle\n");
+    scanf("%f",&radius);
+    area_circle = 3.14 * radius * radius;
+    circumference_circle = 2*3.14*radius;
+    printf("the area of the circle is %f and the circumference of the circle is %f",area_circle,circumference_circle);
     return 0;
 }

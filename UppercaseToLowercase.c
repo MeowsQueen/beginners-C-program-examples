08bade40bbd11e58fdd571d4286c7b6ad7abef3d
MeowsQueen <117544573+MeowsQueen@users.noreply.github.com>
2022-11-07 00:17:12 +0300

    Update UppercaseToLowercase.c

--- UppercaseToLowercase.c
+++ UppercaseToLowercase.c
@@ -1,14 +1,12 @@
 // Uppercase character to lowercase character
 
+#include <stdio.h>
 
-#include<stdio.h>
-int main()
-{
-    char a,u;
-    printf("Enter Uppercase letter :\n");
-    scanf("%c", &a);
-    u = a + 32;
-    printf("Lowercase is : %c", u);
+int main() {
+    char uppercase,lowercase;
+    printf("enter an uppercase character\n");
+    scanf("%c",&uppercase);
+    lowercase = uppercase + 32;
+    printf("you entered %c the lowercase form is %c\n",uppercase,lowercase);
     return 0;
-
 }

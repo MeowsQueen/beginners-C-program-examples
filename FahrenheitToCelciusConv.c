13bb0b13883358fb39185a627769b906719ecde6
MeowsQueen <117544573+MeowsQueen@users.noreply.github.com>
2022-11-07 00:06:52 +0300

    Update FahrenheitToCelciusConv.c

--- FahrenheitToCelciusConv.c
+++ FahrenheitToCelciusConv.c
@@ -1,12 +1,12 @@
 // Fahrenheit to celcius temp converter
 
-#include<stdio.h>
-int main()
-{
-        float c, f;
-        printf("Enter temp in fahrenheit :\n");
-        scanf("%f", &f);
-        c = (f-32)*5/9;
-        printf("Temp in celcius is : %f",c);
+#include <stdio.h>
 
+int main() {
+    float fahrenheit,celsius;
+    printf("enter a fahrenheit value to convert celsius one\n");
+    scanf("%f",&fahrenheit);
+    celsius = (fahrenheit - 32) / 1.8;
+    printf("you entered %.2f F which corresponds to %.2f C",fahrenheit,celsius);
+    return 0;
 }
